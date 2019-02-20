8bfc066ea7a1434a4c0b2f2a7e7d676fcad89986
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-20 11:45:00 +0300

    Update All_Tasks.ps1

--- All_Tasks.ps1
+++ All_Tasks.ps1
@@ -1,4 +1,4 @@
-#1.1.	Вывести все IP адреса вашего компьютера (всех сетевых интерфейсов)
+﻿#1.1.	Вывести все IP адреса вашего компьютера (всех сетевых интерфейсов)
 Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE -ComputerName . | Select-Object -Property IPAddress
 
 #1.2.	Получить mac-адреса всех сетевых устройств вашего компьютера 
@@ -20,6 +20,29 @@ Invoke-Command -ScriptBlock {Get-WmiObject -Class Win32_NetworkAdapterConfigurat
 
 # 1.5.	Удалить шару из п.1.4
 (Get-WmiObject -Class Win32_Share -ComputerName . -Filter "Name='TempShare'").InvokeMethod("Delete",$null)
+
+#1.6.	Скрипт входными параметрами которого являются Маска подсети и два ip-адреса. 
+#Результат  – сообщение (ответ) в одной ли подсети эти адреса.
+# Извините, на доведение красоты и защиты от дурака совсем не было времени(((
+
+$ad1= (Read-Host "Enter IP ADD1").Split('.') #после ввода получаем строку IP без точек
+$ad2= (Read-Host "Enter IP ADD2").Split('.')  #после ввода получаем строку IP без точек  
+$netm= (Read-Host "Enter Network mask").Split('.') #после ввода получаем строку маски без точек
+
+$net1=for ($i=0; $i -lt 4; $i++){
+    $ad1[$i] -band $netm[$i]
+} #получаем адрес первой сети 
+$net2=for ($i=0; $i -lt 4; $i++){
+    $ad2[$i] -band $netm[$i]
+} #получаем адрес второй сети
+ 
+if (($net1[0] -eq $net2[0]) -and ($net1[1] -eq $net2[1]) -and ($net1[2] -eq $net2[2]) -and ($net1[3] -eq $net2[3]))
+    {
+    Write-Host "It's one networks"
+}
+else {
+    write-host "It's different networks"
+}
 
 # 2.1.	Получить список коммандлетов работы с Hyper-V (Module Hyper-V)
 Get-Command -module "*Hyper*"
